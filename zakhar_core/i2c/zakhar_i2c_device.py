b81008d06c720a771e43e0f3a767504d71157599
an-dr <mail@agramakov.me>
2020-04-11 08:10:35 +0000

    face module support

--- zakhar_core/i2c/zakhar_i2c_device.py
+++ zakhar_core/i2c/zakhar_i2c_device.py
@@ -8,8 +8,24 @@ class ZakharI2cDevice:
     def write_byte_to(self, reg, val):
         self.bus.write_byte_data(self.address, reg, val)
 
+    def write_byte_and_verify(self, reg, val):
+        self.write_byte_to(reg, val)
+        while (self.read_byte_from(reg) != val):
+            self.write_byte_to(reg, val)
+            sleep(0.01) # wait
+
     def write_bytes_to(self, reg, vals):
         self.bus.write_i2c_block_data(self.address, reg, vals)
+
+    def send_cmd(self, val):
+        # clear state
+        self.write_byte_and_verify(REG_CMD, CMD_NONE)
+        # write cmd
+        self.write_byte_to(REG_CMD,val)
+        # wait until done
+        while (self.read_byte_from(REG_CMD) != CMD_DONE):
+            sleep(0.01) # wait
+
 
     def read_byte_from(self, reg):
         self.bus.write_byte(self.address, reg)
